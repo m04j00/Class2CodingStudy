6d302556c2dccc023cafc2e22b38b92b37a568d3
Lee MinJi <s2035@e-mirim.hs.kr>
2021-05-08 22:42:48 +0900

    step 3 - 1, 2

---
+++ BaekJoon/Project/Project/no3-1_2739.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+
+	cin >> num;
+
+	for (int i = 1; i <= 9; i++) {
+		cout << num << " * " << i << " = " << i * num << endl;
+	}
+	return 0;
+}
