b903f1dda3b1a35eca439046e3dda035b6ce9966
Lee MinJi <s2035@e-mirim.hs.kr>
2021-05-18 00:08:32 +0900

    step 5 - 7

--- BaekJoon/Project/Project/no5-7_4344.cpp
+++ BaekJoon/Project/Project/no5-7_4344.cpp
@@ -4,5 +4,26 @@ using namespace std;
 int main() {
 	int c;
 	cin >> c;
+	int nArray[1000];
 
+	for (int i = 0; i < c; i++) {
+		int n, sum = 0;
+		double avg = 1;
+
+		cin >> n;
+		for (int j = 0; j < n; j++) {
+			cin >> nArray[j];
+			sum += nArray[j];
+		}
+
+		avg = sum / n;
+		int cnt = 0;
+
+		for (int j = 0; j < n; j++) {
+			if (nArray[j] > avg) cnt++;
+		}
+		avg = (double)cnt / n * 100;
+		printf("%0.3f%%\n", avg);
+	}
+	return 0;
 }
