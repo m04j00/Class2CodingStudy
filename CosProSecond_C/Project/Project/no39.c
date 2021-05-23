b2d4f13ad88e67689bece8204d4da9691df5b44f
LEE MINJI <m04j00@gmail.com>
2021-05-23 15:30:16 +0900

    no 39

--- CosProSecond_C/Project/Project/no39.c
+++ CosProSecond_C/Project/Project/no39.c
@@ -17,9 +17,9 @@ int solution(int height[][4], int height_len) {
 }
 
 int main() {
-    int height[4][4] = { {3, 6, 2, 8}, {7, 3, 4, 2}, {8, 6, 7, 3}, {5, 3, 2, 9} };
-    int height_len = 4;
-    int ret = solution(height, height_len = 4);
+	int height[4][4] = { {3, 6, 2, 8}, {7, 3, 4, 2}, {8, 6, 7, 3}, {5, 3, 2, 9} };
+	int height_len = 4;
+	int ret = solution(height, height_len = 4);
 
-    printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
+	printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
 }
