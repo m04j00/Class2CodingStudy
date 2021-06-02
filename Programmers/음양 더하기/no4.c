61d4ecb91cd4e15ec8a207140805ad13533cabd3
LEE MINJI <m04j00@gmail.com>
2021-06-02 19:52:34 +0900

    no 4

---
+++ Programmers/음양 더하기/no4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int absolutes[], size_t absolutes_len, bool signs[], size_t signs_len) {
+    int answer = 0;
+    for(int i = 0; i < absolutes_len; i++){
+        if(signs[i] == false) absolutes[i] = -absolutes[i];
+        answer += absolutes[i];
+    }
+    
+    return answer;
+}
