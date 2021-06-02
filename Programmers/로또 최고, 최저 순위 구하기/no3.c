8b4d7c6eaebc0d97a55afd9d30034ded70ee59cb
LEE MINJI <m04j00@gmail.com>
2021-06-02 20:25:20 +0900

    day 2

--- Programmers/로또 최고, 최저 순위 구하기/no3.c
+++ Programmers/로또 최고, 최저 순위 구하기/no3.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
 
 int* solution(int lottos[], size_t lottos_len, int win_nums[], size_t win_nums_len) {
--- Programmers/음양 더하기/no4.c
+++ Programmers/음양 더하기/no4.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
 
 int solution(int absolutes[], size_t absolutes_len, bool signs[], size_t signs_len) {
